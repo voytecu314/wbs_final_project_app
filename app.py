2d4eb68ab73a6eb5f0166cd2dc575e88889cd90b
Wojtek Urbanski <voytecu@gmail.com>
2025-11-02 21:11:30 +0100

    feat: add Hand Signs Recognition page

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
+from st_components.hand_signs_recognition import render_hand_signs_recognition
 from st_components.home import render_home
 from st_components.rag import render_rag_chat
 
 # Shared sidebar
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "RAG Chat"])
+page = st.sidebar.radio("Go to", ["Home", "RAG Chat", "Hand Signs Recognition"])
 
 # Page routing
 if page == "Home":
     render_home()
 elif page == "RAG Chat":
     render_rag_chat()
+elif page == "Hand Signs Recognition":
+    render_hand_signs_recognition()
