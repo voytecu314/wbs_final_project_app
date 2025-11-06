b367f8868208723a98c46b3d694f035cedfafed6
HemaGunasekaran <hema.vjk@gmail.com>
2025-11-06 14:36:20 +0530

    feat: working on hemas page

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
+from st_components.hema_page import render_hema_page
 from st_components.home import render_home
 from st_components.rag import render_rag_chat
 
 # Shared sidebar
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "RAG Chat"])
+page = st.sidebar.radio("Go to", ["Home", "RAG Chat", "hema_trail"])
 
 # Page routing
 if page == "Home":
     render_home()
 elif page == "RAG Chat":
     render_rag_chat()
+elif page == "hema_trail":
+    render_hema_page()
