7f797d41e240edebe46922cdc3b77655b7a7ffe7
Wojtek Urbanski <voytecu@gmail.com>
2025-11-16 20:21:49 +0100

    chore: changing translation logic and adding country flags base64

--- st_components/sub_components/highscore_redis.py
+++ st_components/sub_components/highscore_redis.py
@@ -137,7 +137,7 @@ def main():
         })
     
     df = pd.DataFrame(table_data)
-    st.dataframe(df, use_container_width=True, hide_index=True)
+    st.dataframe(df, width="stretch", hide_index=True)
 
     st.divider()
 
